28ba55623561098caad955b5af9bab3d811cbcd9
venusv3991 <42146559+venusv3991@users.noreply.github.com>
2018-08-15 13:59:33 -0400

    Avance video 24

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Product;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // model factories
+        factory(Product::class, 100)->create();
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,21 +63,22 @@
         </div>
     </div>
         <div class="section text-center">
-            <h2 class="title">Here is our team</h2>
+            <h2 class="title">Productos disponibles</h2>
             <div class="team">
                 <div class="row">
+               @foreach ($products as $product)
                 <div class="col-md-4">
                     <div class="team-player">
                     <div class="card card-plain">
                         <div class="col-md-6 ml-auto mr-auto">
                         <img src="{{ asset ('img/faces/avatar.jpg')}}" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
                         </div>
-                        <h4 class="card-title">Gigi Hadid
+                        <h4 class="card-title">{{$product->name}}</h4>
                         <br>
                         <small class="card-description text-muted">Model</small>
                         </h4>
                         <div class="card-body">
-                        <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some
+                        <p class="card-description">{{$product->description}}</p>
                             <a href="#">links</a> for people to be able to follow them outside the site.</p>
                         </div>
                         <div class="card-footer justify-content-center">
@@ -88,6 +89,7 @@
                     </div>
                     </div>
                 </div>
+                @endforeach  
                 <div class="col-md-4">
                     <div class="team-player">
                     <div class="card card-plain">
@@ -109,6 +111,7 @@
                     </div>
                     </div>
                 </div>
+                 
                 <div class="col-md-4">
                     <div class="team-player">
                     <div class="card card-plain">

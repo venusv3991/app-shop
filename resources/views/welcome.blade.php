dd6d268b47bbd7f217a15c3dc19e2d5ef8200b08
venusv3991 <42146559+venusv3991@users.noreply.github.com>
2018-08-16 13:53:08 -0400

    Rollback a Avance video 24
    Signed-off-by: venusv3991 <42146559+venusv3991@users.noreply.github.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,22 +63,21 @@
         </div>
     </div>
         <div class="section text-center">
-            <h2 class="title">Productos disponibles</h2>
+            <h2 class="title">Here is our team</h2>
             <div class="team">
                 <div class="row">
-               @foreach ($products as $product)
                 <div class="col-md-4">
                     <div class="team-player">
                     <div class="card card-plain">
                         <div class="col-md-6 ml-auto mr-auto">
                         <img src="{{ asset ('img/faces/avatar.jpg')}}" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
                         </div>
-                        <h4 class="card-title">{{$product->name}}</h4>
+                        <h4 class="card-title">Gigi Hadid
                         <br>
                         <small class="card-description text-muted">Model</small>
                         </h4>
                         <div class="card-body">
-                        <p class="card-description">{{$product->description}}</p>
+                        <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some
                             <a href="#">links</a> for people to be able to follow them outside the site.</p>
                         </div>
                         <div class="card-footer justify-content-center">
@@ -89,7 +88,6 @@
                     </div>
                     </div>
                 </div>
-                @endforeach  
                 <div class="col-md-4">
                     <div class="team-player">
                     <div class="card card-plain">
@@ -111,7 +109,6 @@
                     </div>
                     </div>
                 </div>
-                 
                 <div class="col-md-4">
                     <div class="team-player">
                     <div class="card card-plain">
